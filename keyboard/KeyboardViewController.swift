53955a9ac62c0ca2bb5735fcc8fdc180495a68e2
Frank Travieso <ftravieso@consultec-ti.com>
2017-05-09 16:34:14 -0400

    changes

--- keyboard/KeyboardViewController.swift
+++ keyboard/KeyboardViewController.swift
@@ -8,45 +8,64 @@
 
 import UIKit
 
-class KeyboardViewController: UIInputViewController {
+class KeyboardViewController: UIInputViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
     @IBOutlet var nextKeyboardButton: UIButton!
     var emojis = [URL]()
     var myArray = [UIImage]()
     
+    
     override func updateViewConstraints() {
         super.updateViewConstraints()
+        print("le")
         
         // Add custom view sizing constraints here
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-//        let fileManager = FileManager.default
-//        let path = fileManager.containerURL(forSecurityApplicationGroupIdentifier: "group.frank.meegTest")?.appendingPathComponent("emojis")
-//        
-//        do {
-//            
-//            let directoryContents = try FileManager.default.contentsOfDirectory(at: path!, includingPropertiesForKeys: nil, options: [])
-//            
-//            print(directoryContents)
-//            
-//        } catch  {
-//            
-//        }
+        print("epa")
         
         getEmojis()
         setEmojis()
         //print(myArray.count)
         
-        let imageTest = UIImageView()
+//        let imageTest = UIImageView()
+//        
+//        imageTest.image = myArray[0]
+//        imageTest.sizeToFit()
+//        imageTest.translatesAutoresizingMaskIntoConstraints = false
+//        
+//        self.view.addSubview(imageTest)
+        let lBundle = Bundle(for: type(of: self))
+        let lCellNib = UINib(nibName: "KeyboardKeyCollectionViewCell", bundle: lBundle)
         
-        imageTest.image = myArray[0]
-        imageTest.sizeToFit()
-        imageTest.translatesAutoresizingMaskIntoConstraints = false
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 1, left: 1, bottom: 1, right: 1)
+        layout.itemSize = CGSize(width: 20, height: 20)
+        layout.minimumInteritemSpacing = 1
+        layout.minimumLineSpacing = 1
+        layout.scrollDirection = .horizontal
         
-        self.view.addSubview(imageTest)
+        print(layout.itemSize)
+        
+        let mCollectionView: UICollectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
+        
+        mCollectionView.isPagingEnabled = true
+        mCollectionView.contentOffset.x = 0.0
+        
+        mCollectionView.dataSource = self
+        mCollectionView.delegate = self
+        
+        mCollectionView.register(lCellNib, forCellWithReuseIdentifier: "Cell")
+        
+        mCollectionView.sizeToFit()
+        mCollectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.view.addSubview(mCollectionView)
+        
+        mCollectionView.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
+        mCollectionView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
         
         // Perform custom UI setup here
         self.nextKeyboardButton = UIButton(type: .system)
@@ -55,8 +74,8 @@ class KeyboardViewController: UIInputViewController {
         self.nextKeyboardButton.sizeToFit()
         self.nextKeyboardButton.translatesAutoresizingMaskIntoConstraints = false
         
-        self.nextKeyboardButton.addTarget(self, action: #selector(handleInputModeList(from:with:)), for: .allTouchEvents)
-        
+        //self.nextKeyboardButton.addTarget(self, action: #selector(handleInputModeList(from:with:)), for: .allTouchEvents)
+        self.nextKeyboardButton.addTarget(self, action: #selector(UIInputViewController.advanceToNextInputMode), for: .touchUpInside)
         self.view.addSubview(self.nextKeyboardButton)
         
         self.nextKeyboardButton.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
@@ -109,11 +128,24 @@ class KeyboardViewController: UIInputViewController {
             
 
                 myArray.append(UIImage(contentsOfFile: $0.path)!)
-                
-            
             
         }
     
     }
     
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
+    {
+        return myArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath as IndexPath) as! KeyboardKeyCollectionViewCell
+        cell.emoji.image = myArray[indexPath.row]
+        return cell
+    }
+    
+}
+
+class KeyboardKeyCollectionViewCell: UICollectionViewCell {
+    var emoji: UIImageView!
 }
