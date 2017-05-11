569a8864b29b6dabc68701a66c5d9a11cbe17681
Frank Travieso <ftravieso@consultec-ti.com>
2017-05-11 11:52:23 -0400

    keyboard with storyboard

---
+++ keyboard/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  meegTest
+//
+//  Created by admin on 5/11/17.
+//  Copyright © 2017 frank. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var emoji: UIImageView!
+    
+}
--- keyboard/KeyboardViewController.swift
+++ keyboard/KeyboardViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class KeyboardViewController: UIInputViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
+    @IBOutlet weak var collectionEmojis: UICollectionView!
     @IBOutlet var nextKeyboardButton: UIButton!
     var emojis = [URL]()
     var myArray = [UIImage]()
@@ -28,20 +29,21 @@ class KeyboardViewController: UIInputViewController, UICollectionViewDelegate, U
         
         getEmojis()
         setEmojis()
+        setCollectionEmojisLayout()
         
         // Perform custom UI setup here
         self.nextKeyboardButton = UIButton(type: .system)
       
         self.nextKeyboardButton.setTitle(NSLocalizedString("Next Keyboard", comment: "Title for 'Next Keyboard' button"), for: [])
-//        self.nextKeyboardButton.sizeToFit()
-//        self.nextKeyboardButton.translatesAutoresizingMaskIntoConstraints = false
+        self.nextKeyboardButton.sizeToFit()
+        self.nextKeyboardButton.translatesAutoresizingMaskIntoConstraints = false
         
         //self.nextKeyboardButton.addTarget(self, action: #selector(handleInputModeList(from:with:)), for: .allTouchEvents)
         self.nextKeyboardButton.addTarget(self, action: #selector(UIInputViewController.advanceToNextInputMode), for: .touchUpInside)
-//        self.view.addSubview(self.nextKeyboardButton)
-//        
-//        self.nextKeyboardButton.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
-//        self.nextKeyboardButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+        self.view.addSubview(self.nextKeyboardButton)
+        
+        self.nextKeyboardButton.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
+        self.nextKeyboardButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
     }
     
     override func didReceiveMemoryWarning() {
@@ -101,15 +103,45 @@ class KeyboardViewController: UIInputViewController, UICollectionViewDelegate, U
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath as IndexPath) as! KeyboardKeyCollectionViewCell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath as IndexPath) as! CollectionViewCell
         cell.emoji.image = myArray[indexPath.row]
         return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("seleccionaste el emoji \(indexPath.row)")
+        
+        let image = myArray[indexPath.row]
+        
+        let pb = UIPasteboard.general
+        
+        let pngType = UIPasteboardTypeListImage[0] as! String
+        
+        pb.image = image
+        
+        pb.setData(UIImagePNGRepresentation(image)!, forPasteboardType: pngType)
     }
     
     
     @IBAction func next(_ sender: Any) {
         print("ejele")
         self.advanceToNextInputMode()
+        
+    }
+    
+    func setCollectionEmojisLayout(){
+        
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        layout.itemSize = CGSize(width: self.view.bounds.width/4, height: self.view.bounds.height/2)
+        layout.minimumInteritemSpacing = 0
+        layout.minimumLineSpacing = 0
+        layout.scrollDirection = .horizontal
+        
+        self.collectionEmojis!.collectionViewLayout = layout
+        
+        collectionEmojis.isPagingEnabled = true
+        collectionEmojis.contentOffset.x = 0.0
         
     }
     
