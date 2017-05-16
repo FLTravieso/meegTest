fd6c280811d6cd5785e74dc6d6c801755b8e1ed4
Frank Travieso <frankltp9412@gmail.com>
2017-05-15 23:29:45 -0400

    keyboard

--- keyboard/KeyboardViewController.swift
+++ keyboard/KeyboardViewController.swift
@@ -11,64 +11,61 @@ import UIKit
 class KeyboardViewController: UIInputViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
     @IBOutlet weak var collectionEmojis: UICollectionView!
-    @IBOutlet var nextKeyboardButton: UIButton!
+    @IBOutlet weak var deleteButton: UIButton!
+    @IBOutlet weak var pager: UIPageControl!
+    @IBOutlet weak var hintView: UIView!
+    @IBOutlet weak var hintButton: UIButton!
     var emojis = [URL]()
     var myArray = [UIImage]()
-    
+    var constraint = NSLayoutConstraint()
+
+    let kPressPasteTitleAppearDuration = 0.5
     
     override func updateViewConstraints() {
         super.updateViewConstraints()
-        setCollectionEmojisLayout()
-        print("le")
         
-        // Add custom view sizing constraints here
+        setKeyboardHeight()
+        
+        setCollectionEmojisLayout()
+        
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        
+        setKeyboardHeight()
+
+        
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("epa")
         
         getEmojis()
         setEmojis()
+        setNumberOfPages()
         
+        self.deleteButton.setTitle("⌫", for: .normal)
         
-        // Perform custom UI setup here
-        self.nextKeyboardButton = UIButton(type: .system)
-      
-        self.nextKeyboardButton.setTitle(NSLocalizedString("Next Keyboard", comment: "Title for 'Next Keyboard' button"), for: [])
-        self.nextKeyboardButton.sizeToFit()
-        self.nextKeyboardButton.translatesAutoresizingMaskIntoConstraints = false
-        
-        //self.nextKeyboardButton.addTarget(self, action: #selector(handleInputModeList(from:with:)), for: .allTouchEvents)
-        self.nextKeyboardButton.addTarget(self, action: #selector(UIInputViewController.advanceToNextInputMode), for: .touchUpInside)
-        self.view.addSubview(self.nextKeyboardButton)
-        
-        self.nextKeyboardButton.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
-        self.nextKeyboardButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+        hintView.isHidden = true
+        configureHintButton()
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated
-    }
-    
-    override func textWillChange(_ textInput: UITextInput?) {
-        // The app is about to change the document's contents. Perform any preparation here.
-    }
-    
-    override func textDidChange(_ textInput: UITextInput?) {
-        // The app has just changed the document's contents, the document context has been updated.
+    func setNumberOfPages(){
         
-        var textColor: UIColor
-        let proxy = self.textDocumentProxy
-        if proxy.keyboardAppearance == UIKeyboardAppearance.dark {
-            textColor = UIColor.white
-        } else {
-            textColor = UIColor.blue
-        }
-        self.nextKeyboardButton.setTitleColor(textColor, for: [])
+        let pageNumber = Int(ceilf(Float(myArray.count)/Float(8)))
+        
+        self.pager.numberOfPages = pageNumber
+        
     }
     
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        
+        let pageNumber = round(scrollView.contentOffset.x / scrollView.frame.size.width)
+        self.pager.currentPage = Int(pageNumber)
+        
+        print(pageNumber)
+        
+    }
     
     func getEmojis() {
         
@@ -121,22 +118,39 @@ class KeyboardViewController: UIInputViewController, UICollectionViewDelegate, U
         pb.image = image
         
         pb.setData(UIImagePNGRepresentation(image)!, forPasteboardType: pngType)
+        
+        showHint()
     }
     
     
     @IBAction func next(_ sender: Any) {
-        print("ejele")
+
         self.advanceToNextInputMode()
         
     }
     
+    
+    @IBAction func deleteEmoji(_ sender: Any) {
+        
+        let proxy = textDocumentProxy as UITextDocumentProxy
+        
+        proxy.deleteBackward()
+    }
+
+    
+    
     func setCollectionEmojisLayout(){
+
+        
+        print(self.view.bounds.width/4)
+        
+        self.automaticallyAdjustsScrollViewInsets = false
         
         let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
-        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-        layout.itemSize = CGSize(width: self.view.bounds.width/4, height: self.view.bounds.height/2 - 1)
-        layout.minimumInteritemSpacing = 1
-        layout.minimumLineSpacing = 1
+        layout.sectionInset = UIEdgeInsets(top: 5, left: 0, bottom: 5, right: 0)
+        layout.itemSize = CGSize(width: 80, height: 80)
+        layout.minimumInteritemSpacing = 0
+        layout.minimumLineSpacing = 0
         layout.scrollDirection = .horizontal
         
         self.collectionEmojis!.collectionViewLayout = layout
@@ -146,8 +160,81 @@ class KeyboardViewController: UIInputViewController, UICollectionViewDelegate, U
         
     }
     
-}
+    func setKeyboardHeight () {
+        
+        let screenSize = UIScreen.main.bounds.size
+        let screenH = screenSize.height
+        
+        self.view.removeConstraint(constraint)
+        
+        if screenH >= 414 {
+            
+            //for iPhone portrait AND iPhone Plus landscape or portrait
+            self.constraint = NSLayoutConstraint(item: self.view, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 0.0, constant: 220.0)
+            self.view.addConstraint(self.constraint)
+            
+            
+        } else {
+            //for iPhone landscape
+            self.constraint = NSLayoutConstraint(item: self.view, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 0.0, constant: 140.0)
+            self.view.addConstraint(self.constraint)
 
-class KeyboardKeyCollectionViewCell: UICollectionViewCell {
-    var emoji: UIImageView!
+        }
+        
+    }
+    
+    fileprivate func configureHintButton() {
+        hintView.backgroundColor = UIColor(red: 32/255.0, green: 59/255.0, blue: 104/255.0, alpha: 0.7)
+        
+        let fontSize:CGFloat = 16.0
+        let button = UIButton(frame: CGRect(x:0, y: 0, width: 140, height: 100))
+        hintButton = button
+        let font1 = UIFont(name: "Arial", size: fontSize) ?? UIFont.systemFont(ofSize: fontSize)
+        let font2 = UIFont(name: "Arial", size:fontSize) ?? UIFont.systemFont(ofSize: fontSize)
+        let color = UIColor.white
+        
+        let myNormalAttributedTitle = NSMutableAttributedString(string: "Copied to the clipboard. Now tap and\n",
+                                                                attributes: [NSFontAttributeName : font1, NSForegroundColorAttributeName: color ])
+        
+        let myBoldAttributedTitle = NSAttributedString(string: " paste ",
+                                                       attributes: [NSFontAttributeName : font2, NSForegroundColorAttributeName: color])
+        let myLastAttributedTitle = NSAttributedString(string: " into the message field",
+                                                       attributes: [NSFontAttributeName : font1, NSForegroundColorAttributeName: color])
+        myNormalAttributedTitle.append(myBoldAttributedTitle)
+        myNormalAttributedTitle.append(myLastAttributedTitle)
+        hintButton.translatesAutoresizingMaskIntoConstraints = false
+        hintButton.setAttributedTitle(myNormalAttributedTitle, for: .normal)
+        
+        
+        hintButton.layer.cornerRadius = 10
+        hintButton.titleLabel!.numberOfLines = 2
+        hintButton.titleLabel!.lineBreakMode = .byWordWrapping;
+        hintButton.titleLabel!.textAlignment = .center;
+        
+        
+        hintView.addSubview(hintButton)
+        
+        let centerX = NSLayoutConstraint(item: hintView, attribute: .centerX, relatedBy: .equal, toItem: hintButton, attribute: .centerX, multiplier: 1, constant: 0)
+        
+        let centerY = NSLayoutConstraint(item: hintView, attribute: .centerY, relatedBy: .equal, toItem: hintButton, attribute: .centerY, multiplier: 1, constant: 0)
+        
+        NSLayoutConstraint.activate([centerX, centerY])
+    }
+    
+    fileprivate func hideHint(){
+        self.hintView.isHidden = true
+    }
+    
+    fileprivate func showHint(){
+        self.hintView.isHidden = false
+        self.hintView.alpha = 0
+        UIView.animate(withDuration: kPressPasteTitleAppearDuration, delay: 0.0, options: UIViewAnimationOptions.curveEaseOut, animations: {
+            self.hintView!.alpha = 1
+        }, completion: { (Bool) -> () in
+            UIView.animate(withDuration: self.kPressPasteTitleAppearDuration, delay: self.kPressPasteTitleAppearDuration, options: UIViewAnimationOptions.curveEaseOut, animations: {
+                self.hintView!.alpha = 0
+            }, completion: nil)
+        })
+    }
+    
 }
